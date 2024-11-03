d270eb091c53449b062b89da68c6c13f556c1664
Gassayping <Dev_Gassayping@proton.me>
2024-11-03 14:41:51 -0800

    Formatting: Removed extra newline

--- src/main/java/sandbuckets/Sandbuckets.java
+++ src/main/java/sandbuckets/Sandbuckets.java
@@ -10,7 +10,6 @@ public class Sandbuckets implements ModInitializer {
 	public static final String MOD_ID = "sand-buckets";
 	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
-
 	@Override
 	public void onInitialize() {
 		BucketManager.RegisterItems();
